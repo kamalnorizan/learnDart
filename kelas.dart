07616941952e3089f19a7394e8d1bf8ed43b5f4f
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2022-09-26 12:30:40 +0800

    Create Instances

--- kelas.dart
+++ kelas.dart
@@ -22,4 +22,15 @@ class Pelajar {
   Pelajar(this.name, {this.jantina, this.tarikhLahir, this.kelasSemasa});
 }
 
-void main() {}
+void main() {
+  Kelas bestari = Kelas('Bestari',
+      kapasiti: 25, jumlahKipas: 2, guruKelas: GuruKelas('Amira', umur: 30));
+  Kelas cemerlang = Kelas('Cemerlang',
+      kapasiti: 23, jumlahKipas: 1, guruKelas: GuruKelas('Amirul', umur: 35));
+
+  Pelajar pelajar1 = Pelajar('Shafik',
+      jantina: 'Lelaki', kelasSemasa: bestari, tarikhLahir: '20/05/2015');
+
+  Pelajar pelajar2 = Pelajar('Saiful',
+      jantina: 'Lelaki', kelasSemasa: cemerlang, tarikhLahir: '20/07/2015');
+}
