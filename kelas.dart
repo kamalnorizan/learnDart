7bf56fa96fcddd65f02a64e4d56e045156ba5a11
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2022-09-26 15:41:53 +0800

    list and loop

--- kelas.dart
+++ kelas.dart
@@ -3,8 +3,10 @@ class Kelas {
   int? kapasiti;
   int? jumlahKipas;
   GuruKelas? guruKelas;
+  List<Pelajar>? senaraiPelajar = [];
 
-  Kelas(this.nama, {this.kapasiti, this.jumlahKipas, this.guruKelas});
+  Kelas(this.nama,
+      {this.kapasiti, this.jumlahKipas, this.guruKelas, this.senaraiPelajar});
 }
 
 class GuruKelas {
@@ -23,6 +25,7 @@ class Pelajar {
 }
 
 void main() {
+  List<Pelajar> senarai = [];
   Kelas bestari = Kelas('Bestari',
       kapasiti: 25, jumlahKipas: 2, guruKelas: GuruKelas('Amira', umur: 30));
   Kelas cemerlang = Kelas('Cemerlang',
@@ -32,5 +35,21 @@ void main() {
       jantina: 'Lelaki', kelasSemasa: bestari, tarikhLahir: '20/05/2015');
 
   Pelajar pelajar2 = Pelajar('Saiful',
-      jantina: 'Lelaki', kelasSemasa: cemerlang, tarikhLahir: '20/07/2015');
+      jantina: 'Lelaki', kelasSemasa: bestari, tarikhLahir: '20/07/2015');
+
+  senarai.add(pelajar1);
+  senarai.add(pelajar2);
+  bestari.senaraiPelajar = senarai;
+
+  for (var pelajar in bestari.senaraiPelajar!) {
+    print(pelajar.name.toString() +
+        ', ' +
+        pelajar.kelasSemasa!.nama.toString() +
+        ', ' +
+        pelajar.kelasSemasa!.guruKelas!.name.toString());
+  }
+
+//   print(pelajar1.name.toString() +', '+
+//         pelajar1.kelasSemasa!.nama.toString()+', '+
+//         pelajar1.kelasSemasa!.guruKelas!.name.toString());
 }
